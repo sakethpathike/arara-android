c852b4fa0101b7d2e44a49ea2e60473f27849230
SakethPathike <sakethpathike@gmail.com>
2022-07-24 15:04:11 +0530

    small changes

--- app/src/main/java/com/sakethh/arara/unreleased/songThing.kt
+++ app/src/main/java/com/sakethh/arara/unreleased/songThing.kt
@@ -10,6 +10,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.sakethh.arara.R
@@ -30,19 +31,39 @@ fun SongThing(imageLink: String) {
             .border(width = 3.dp, color = Color.DarkGray)
             .background(color = Color.Transparent)
     ) {
-      Row(modifier = Modifier.fillMaxSize()) {
-          AsyncImage(
-              model = imageLink,
-              contentDescription = "Image for unreleased song from a warrior:)",
-              modifier = Modifier
-                  .padding(top = 10.dp) //gives 10dp padding in top
-                  .height(40.dp) // renders height of the image
-                  .padding(start = 10.dp) //gives 10dp padding in left
-                  .width(40.dp) /*renders width of the image*/,
-              error = painterResource(R.drawable.image)
-          )
-          Text(modifier = Modifier.padding(top = 10.dp), text = "Sample Text Bolte!")
-      }
+        Row(modifier = Modifier.fillMaxSize()) {
+            AsyncImage(
+                model = imageLink,
+                contentDescription = "Image for unreleased song from a warrior:)",
+                modifier = Modifier
+                    .padding(top = 10.dp) //gives 10dp padding in top
+                    .requiredHeight(40.dp) // renders height of the image
+                    .padding(start = 10.dp) //gives 10dp padding in left
+                    .requiredWidth(40.dp) /*renders width of the image*/,
+                error = painterResource(R.drawable.image)
+            )
+            Column(
+                modifier = Modifier
+                    .padding(top = 15.dp, bottom = 15.dp)
+                    .fillMaxHeight()
+                    .wrapContentWidth()
+            ) {
+                Text(
+                    text = "Shelff!",
+                    modifier = Modifier.requiredSize(10.dp),
+                    fontFamily = FontFamily.Monospace,
+                    color = Color.DarkGray
+                )
+                Text(
+                    text = "Lyrics",
+                    color = Color.Gray,
+                    modifier = Modifier
+                        .background(color = Color.LightGray)
+                        .requiredSize(5.dp),
+                    fontFamily = FontFamily.Monospace
+                )
+            }
+        }
 
     }
 }
