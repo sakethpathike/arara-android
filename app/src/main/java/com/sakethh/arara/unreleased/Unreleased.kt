de27276ce5c5f0678948387e1f9b3a4c2c6aab0b
SakethPathike <sakethpathike@gmail.com>
2022-07-29 16:26:04 +0530

    ui results on success and failure

--- app/src/main/java/com/sakethh/arara/unreleased/Unreleased.kt
+++ app/src/main/java/com/sakethh/arara/unreleased/Unreleased.kt
@@ -1,14 +1,13 @@
 package com.sakethh.arara.unreleased
 
 import android.annotation.SuppressLint
+import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -20,20 +19,23 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import coil.request.ImageRequest
 import com.germainkevin.collapsingtopbar.CollapsingTopBar
 import com.germainkevin.collapsingtopbar.CollapsingTopBarColors
 import com.germainkevin.collapsingtopbar.rememberCollapsingTopBarScrollBehavior
+import com.github.kittinunf.fuel.core.await
 import com.github.kittinunf.fuel.httpGet
 import com.github.kittinunf.fuel.json.responseJson
+import com.github.kittinunf.result.Result
 import com.sakethh.arara.R
 import com.sakethh.arara.ui.theme.firstGradient
 import com.sakethh.arara.ui.theme.headerColor
 import com.sakethh.arara.ui.theme.secondGradient
+import kotlinx.coroutines.*
+import kotlin.coroutines.CoroutineContext
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter", "CoroutineCreationDuringComposition")
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, FlowPreview::class, DelicateCoroutinesApi::class)
 @Composable
 fun UnreleasedScreen() {
     val scrollBehavior = rememberCollapsingTopBarScrollBehavior(
@@ -55,12 +57,12 @@ fun UnreleasedScreen() {
             modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
             topBar = {
                 CollapsingTopBar(
-                    scrollBehavior = scrollBehavior  , colors = CollapsingTopBarColors(
+                    scrollBehavior = scrollBehavior, colors = CollapsingTopBarColors(
                         backgroundColor = headerColor,
                         contentColor = headerColor,
                         backgroundColorWhenCollapsingOrExpanding = headerColor,
                         onBackgroundColorChange = {}),
-                 title = {
+                    title = {
                         Text(
                             text = "Unreleased",
                             style = MaterialTheme.typography.labelSmall,
@@ -73,11 +75,7 @@ fun UnreleasedScreen() {
             }
         )
         {
-            LazyColumn(
-                contentPadding = it ,modifier = Modifier.background(
-                    headerColor
-                )
-            )
+            LazyColumn(contentPadding = it, modifier = Modifier.background(headerColor))
             {
                 item {
                     Box(
@@ -107,21 +105,70 @@ fun UnreleasedScreen() {
                         )
                     }
                 }
-
-                val apiCall =
-                    "https://sample-server-side.herokuapp.com/unreleased".httpGet()
-                        .responseJson { request, response, result ->
+               CoroutineScope(Dispatchers.IO).launch {
+                    val fetchData = async {
+                        val apiUrl = "https://sample-server-side.herokuapp.com/unreleased"
+                        val request = apiUrl.httpGet()
+                        request.responseJson { _, _, result ->
                             val doc = result.get().array()
                             items(doc.length()) {
-                                SongThing1(
-                                    imageLink = doc.getJSONObject(it)
-                                        .getString("specificArtwork"),
-                                    songName = doc.getJSONObject(it).getString("songName")
-                                )
+                                val imageLink =
+                                    doc.getJSONObject(it).getString("specificArtwork")
+                                val songName = doc.getJSONObject(it).getString("songName")
+                                when (result) {
+                                    is Result.Success -> {
+                                        SongThing(imageLink = imageLink, songName = songName)
+                                    }
+                                    is Result.Failure -> {
+                                        Toast.makeText(
+                                            LocalContext.current,
+                                            "Uh-oh, something went wrong!",
+                                            Toast.LENGTH_SHORT
+                                        ).show()
+                                    }
+                                }
+
                             }
                         }
-                apiCall.join()
+                    }
+                    fetchData.await()
+                }
             }
+
         }
     }
 }
+/*
+class FirstFragment : Fragment() {
+    private lateinit var requestQueue: RequestQueue
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_first, container, false)
+        requestQueue = Volley.newRequestQueue(this.context)
+        loadImage(view.apiImageView)
+        view.apiBtn.setOnClickListener {
+            loadImage(view.apiImageView)
+        }
+        return view
+    }
+"https://sample-server-side.herokuapp.com/unreleased"
+    private fun apiCall(imageView: ImageView) {
+        lifecycleScope.launch(Dispatchers.IO) {
+            val apiUrl = "https://thatcopy.pw/catapi/rest/"
+            val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, apiUrl, null, {
+                val imageURL=it.getString("url") // -> Will Get Our Cat Image URL
+                imageView.load(imageURL)
+            }, {
+                Log.d("API Request Error", "${it.printStackTrace()}")
+            })
+            requestQueue.add(jsonObjectRequest) // -> Added jsonObjectRequest To requestQueue
+        }
+    }
+    private fun loadImage(imageView: ImageView){
+        lifecycleScope.launch(Dispatchers.Main){
+            apiCall(imageView)
+        }
+    }
+}*/
