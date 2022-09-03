01002ccdb6cd73bb747d25a662d056bf0b8476a7
SakethPathike <sakethpathike@gmail.com>
2022-09-03 19:19:40 +0530

    current music screen in progress

--- app/src/main/java/com/sakethh/arara/unreleased/currentMusicScreen/CurrentUnreleasedMusicScreen.kt
+++ app/src/main/java/com/sakethh/arara/unreleased/currentMusicScreen/CurrentUnreleasedMusicScreen.kt
@@ -1,8 +1,8 @@
 package com.sakethh.arara.unreleased.currentMusicScreen
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
+import androidx.compose.foundation.*
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.*
@@ -12,6 +12,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
@@ -44,11 +46,11 @@ fun CurrentMusicScreen(
             val topSpaceHeader = createRefFor("topSpaceHeader")
             val progressBarSpacer = createRefFor("progressBarSpacer")
             val progressBar = createRefFor("progressBar")
-            val spacerAfterProgressBar = createRefFor("spacerAfterProgressBar")
-            val mediaController = createRefFor("mediaController")
+            val boxAfterProgressBar = createRefFor("boxAfterProgressBar")
             val mediaControllerPlayPauseButton = createRefFor("mediaControllerPlayPauseButton")
             val mediaControllerPreviousButton = createRefFor("mediaControllerPreviousButton")
             val mediaControllerNextButton = createRefFor("mediaControllerNextButton")
+            val lyricsBox = createRefFor("lyricsBox")
             constrain(topSpaceHeader) {
                 top.linkTo(parent.top)
                 start.linkTo(parent.start)
@@ -56,13 +58,15 @@ fun CurrentMusicScreen(
             }
             constrain(artWork) {
                 top.linkTo(topSpaceHeader.bottom)
-                start.linkTo(topSpaceHeader.start)
-                end.linkTo(topSpaceHeader.end)
+                start.linkTo(parent.start)
+                end.linkTo(parent.end)
+                bottom.linkTo(titleAndIcon.top)
             }
             constrain(titleAndIcon) {
                 top.linkTo(artWork.bottom)
-                start.linkTo(artWork.start)
-                end.linkTo(artWork.end)
+                bottom.linkTo(lyricsBox.top)
+                start.linkTo(parent.start)
+                end.linkTo(parent.end)
             }
             constrain(progressBarSpacer) {
                 top.linkTo(titleAndIcon.bottom)
@@ -71,120 +75,109 @@ fun CurrentMusicScreen(
             }
             constrain(progressBar) {
                 top.linkTo(progressBarSpacer.bottom)
-                start.linkTo(progressBarSpacer.start)
-                end.linkTo(progressBarSpacer.end)
-            }
-            constrain(spacerAfterProgressBar) {
-                top.linkTo(progressBar.bottom)
-                start.linkTo(progressBar.start)
-                end.linkTo(progressBar.end)
-            }
-            constrain(mediaController) {
-                top.linkTo(spacerAfterProgressBar.bottom)
-                start.linkTo(spacerAfterProgressBar.start)
-                end.linkTo(spacerAfterProgressBar.end)
-            }
-            constrain(mediaControllerPlayPauseButton) {
-                top.linkTo(parent.bottom)
                 start.linkTo(parent.start)
                 end.linkTo(parent.end)
             }
-            constrain(mediaControllerPreviousButton) {
+            constrain(boxAfterProgressBar){
+                top.linkTo(progressBar.bottom)
+                start.linkTo(parent.start)
+                end.linkTo(parent.end)
+            }
+            constrain(mediaControllerPlayPauseButton){
+                top.linkTo(parent.top)
+                bottom.linkTo(parent.bottom)
+                start.linkTo(parent.start)
+                end.linkTo(parent.end)
+            }
+            constrain(mediaControllerPreviousButton){
                 top.linkTo(mediaControllerPlayPauseButton.top)
                 bottom.linkTo(mediaControllerPlayPauseButton.bottom)
                 end.linkTo(mediaControllerPlayPauseButton.start)
             }
-            constrain(mediaControllerNextButton) {
+            constrain(mediaControllerNextButton){
                 top.linkTo(mediaControllerPlayPauseButton.top)
                 bottom.linkTo(mediaControllerPlayPauseButton.bottom)
                 start.linkTo(mediaControllerPlayPauseButton.end)
             }
-
+            constrain(lyricsBox){
+                top.linkTo(boxAfterProgressBar.bottom)
+                bottom.linkTo(parent.bottom)
+                start.linkTo(parent.start)
+                end.linkTo(parent.end)
+            }
         }
-        ConstraintLayout(
-            constraintSet = constraintsSet,
-            modifier = Modifier
-                .fillMaxSize()
-                .background(color = backgroundColor)
-        ) {
-            Spacer(
-                Modifier
-                    .fillMaxWidth()
-                    .height(50.dp)
-                    .layoutId("topSpaceHeader")
-            )
-            ImageThing(
-                model = ImageRequest.Builder(LocalContext.current)
-                    .data(currentMusicImg)
-                    .crossfade(true)
-                    .build(),
-                contentDescription = "Image Of Current Music Which is Playing",
+            ConstraintLayout(
+                constraintSet = constraintsSet,
                 modifier = Modifier
-                    .requiredSize(300.dp)
-                    .layoutId("artWork")
-                    .shadow(2.dp),
-                onError = painterResource(id = R.drawable.current_music_screen_pic)
-            )
-            Row(
-                modifier = Modifier
-                    .padding(top = 25.dp)
-                    .width(300.dp)
-                    .wrapContentHeight()
-                    .layoutId("titleAndIcon")
+                    .fillMaxSize()
+                    .background(backgroundColor)
+                    .verticalScroll(state = rememberScrollState(), enabled = true)
             ) {
-                Box(
+                Spacer(
+                    Modifier
+                        .fillMaxWidth()
+                        .height(50.dp)
+                        .layoutId("topSpaceHeader")
+                )
+                ImageThing(
+                    model = ImageRequest.Builder(LocalContext.current)
+                        .data(currentMusicImg)
+                        .crossfade(true)
+                        .build(),
+                    contentDescription = "Image Of Current Music Which is Playing",
                     modifier = Modifier
-                        .width(250.dp)
-                        .wrapContentHeight(),
-                    contentAlignment = Alignment.CenterStart
-                ) {
-                    Text(
-                        text = currentSongTitle,
-                        style = MaterialTheme.typography.titleMedium,
-                        color = Color.White,
-                        maxLines = 1,
-                        fontSize = 20.sp,
-                        overflow = TextOverflow.Ellipsis
-                    )
-                }
-                Box(
+                        .requiredSize(300.dp)
+                        .layoutId("artWork")
+                        .shadow(2.dp),
+                    onError = painterResource(id = R.drawable.current_music_screen_pic)
+                )
+                Row(
                     modifier = Modifier
-                        .width(50.dp)
-                        .wrapContentHeight(),
-                    contentAlignment = Alignment.CenterEnd
+                        .width(300.dp)
+                        .wrapContentHeight()
+                        .layoutId("titleAndIcon")
                 ) {
-                    IconButton(onClick = { }, modifier = Modifier.size(25.dp)) {
-                        Image(
-                            painter = painterResource(id = R.drawable.dropdown),
-                            contentDescription = "Description"
+                    Box(
+                        modifier = Modifier
+                            .width(250.dp)
+                            .wrapContentHeight(),
+                        contentAlignment = Alignment.CenterStart
+                    ) {
+                        Text(
+                            text = currentSongTitle,
+                            style = MaterialTheme.typography.titleMedium,
+                            color = Color.White,
+                            maxLines = 1,
+                            fontSize = 20.sp,
+                            overflow = TextOverflow.Ellipsis
                         )
                     }
+                    Box(
+                        modifier = Modifier
+                            .width(50.dp)
+                            .wrapContentHeight(),
+                        contentAlignment = Alignment.CenterEnd
+                    ) {
+                        IconButton(onClick = { }, modifier = Modifier.size(25.dp)) {
+                            Image(
+                                painter = painterResource(id = R.drawable.dropdown),
+                                contentDescription = "Description"
+                            )
+                        }
+                    }
                 }
-            }
-            Spacer(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(15.dp)
-                    .layoutId("progressBarSpacer")
-            )
-            LinearProgressIndicator(
-                modifier = Modifier
-                    .width(300.dp)
-                    .height(5.dp)
-                    .layoutId("progressBar")
-            )
-            Spacer(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(15.dp)
-                    .layoutId("spacerAfterProgressBar")
-            )
-            Row(
-                modifier = Modifier
-                    .width(300.dp)
-                    .wrapContentHeight()
-                    .layoutId("mediaController")
-            ) {
+                Spacer(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(15.dp)
+                        .layoutId("progressBarSpacer")
+                )
+                LinearProgressIndicator(
+                    modifier = Modifier
+                        .width(300.dp)
+                        .height(5.dp)
+                        .layoutId("progressBar")
+                )
                 val currentIconState = currentMusicScreenViewModel.currentIsPauseIcon
                 val currentPlayPauseIcons = currentMusicScreenViewModel.currentPlayPauseIcons
                 val currentImageState = remember { mutableListOf(0, 1) }
@@ -195,44 +188,59 @@ fun CurrentMusicScreen(
                     val pauseIcon = currentPlayPauseIcons[1]
                     currentImageState[0] = pauseIcon
                 }
-
-                IconButton( //previous button
-                    onClick = {
-
-                    }, modifier = Modifier.requiredSize(35.dp).layoutId("mediaControllerPreviousButton")
+                BoxWithConstraints(
+                    modifier = Modifier
+                        .padding(top = 15.dp)
+                        .width(300.dp)
+                        .wrapContentHeight()
+                        .layoutId("boxAfterProgressBar")
                 ) {
-                    Image(
-                        painter = painterResource(id = R.drawable.previoustrack),
-                        modifier = Modifier
-                            .requiredSize(35.dp),
-                        contentDescription = "Play/Pause Icons"
-                    )
+                    ConstraintLayout( modifier = Modifier
+                        .width(300.dp)
+                        .wrapContentHeight(),constraintSet = constraintsSet ) {
+                        IconButton(modifier = Modifier
+                            .layoutId("mediaControllerPreviousButton"),onClick={}){
+                            Image(
+                                painter = painterResource(id = R.drawable.previoustrack),
+                                modifier = Modifier
+                                    .padding(end = 30.dp)
+                                    .requiredSize(35.dp),
+                                contentDescription = "PreviousButton"
+                            )
+                        }
+                        IconButton(
+                            // play||pause buttons
+                            onClick = {
+                                currentIconState.value = !currentIconState.value
+                            },
+                            modifier = Modifier
+                                .requiredSize(60.dp)
+                                .background(color = Color.White, shape = CircleShape)
+
+                                .layoutId("mediaControllerPlayPauseButton"),
+                        ) {
+                            Image(
+                                painter = painterResource(id = currentImageState[0]),
+                                modifier = Modifier
+                                    .requiredSize(30.dp),
+                                contentDescription = "Play/Pause Icons"
+                            )
+                        }
+                       IconButton(onClick = {  }, modifier = Modifier.layoutId("mediaControllerNextButton")) {
+                        Image(
+                            painter = painterResource(id = R.drawable.nexttrack),
+                            modifier = Modifier
+                                .padding(start = 30.dp)
+                                .requiredSize(35.dp),
+                            contentDescription = "NextButton"
+                        )
+                       }
+                    }
                 }
-                IconButton(  // play||pause buttons
-                    onClick = {
-                        currentIconState.value = !currentIconState.value
-                    }, modifier = Modifier.requiredSize(55.dp).background(color=Color.White, shape = CircleShape).layoutId("mediaControllerPlayPauseButton")
-                ) {
-                    Image(
-                        painter = painterResource(id = currentImageState[0]),
-                        modifier = Modifier
-                            .requiredSize(35.dp),
-                        contentDescription = "Play/Pause Icons"
-                    )
-                }
-                IconButton(  // next button
-                    onClick = {
-
-                    }, modifier = Modifier.layoutId("mediaControllerNextButton")
-                ) {
-                    Image(
-                        painter = painterResource(id = R.drawable.nexttrack),
-                        modifier = Modifier
-                            .requiredSize(35.dp),
-                        contentDescription = "Play/Pause Icons"
-                    )
+                // lyrics thing:
+                BoxWithConstraints(modifier = Modifier.width(300.dp).height(300.dp).background(Color.White).border(border=BorderStroke(10.dp,Color.Transparent),shape= RectangleShape).layoutId("lyricsBox")) {
+                    
                 }
             }
         }
     }
-}
