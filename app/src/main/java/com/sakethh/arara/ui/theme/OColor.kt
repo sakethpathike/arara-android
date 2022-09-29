4a62edfb9c2a4121a508e3f184f644acab04db4d
SakethPathike <sakethpathike@gmail.com>
2022-09-29 16:58:56 +0530

    major updates in UI, added HomeScreen.kt and related stuff!

---
+++ app/src/main/java/com/sakethh/arara/ui/theme/OColor.kt
@@ -0,0 +1,18 @@
+package com.sakethh.arara.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val secondGradient=Color(0xFF000000)
+val firstGradient=Color(0xFF434343)
+val backgroundColor= Color(0xFF070606)
+val headerColor= Color(0xD5070606)
+val musicPlayerColor= Color(0xE6070606)
+val customMessageBG=Color(0xFF2F3032)
